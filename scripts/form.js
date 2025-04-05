401c43f2aab880c2ed534b9908e56ff59568e107
baileynance <baieyj.nance@gmail.com>
2025-04-04 19:58:32 -0700

    assignment 5

---
+++ scripts/form.js
@@ -0,0 +1,46 @@
+document.getElementById('lastModified').innerText = new Date(document.lastModified);
+document.getElementById('currentyear').innerText = new Date().getFullYear();
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+// ----------------------------------------------------------
+// Product Name Dropdown
+// ----------------------------------------------------------
+
+const productName = document.getElementById("product-name");
+const displayProductNames = () => {
+    products.forEach(product => {
+        let option = document.createElement("option");
+        option.textContent = product.name;
+        option.setAttribute("value", product.id);
+        productName.appendChild(option);
+    })
+}
+
+displayProductNames();
