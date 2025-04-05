266202f424faa3837737e57276412137cc3147a2
baileynance <baieyj.nance@gmail.com>
2025-04-04 20:11:24 -0700

    local storage added

--- scripts/review.js
+++ scripts/review.js
@@ -1,2 +1,21 @@
 document.getElementById('lastModified').innerText = new Date(document.lastModified);
 document.getElementById('currentyear').innerText = new Date().getFullYear();
+
+// ----------------------------------------------------------
+// Local Storage for Form Data
+// ----------------------------------------------------------
+
+function getSubmissionCount() {
+    return parseInt(localStorage.getItem('submissionCount')) || 0;
+}
+
+function incrementSubmissionCount() {
+    let count = getSubmissionCount();
+    count += 1;
+    localStorage.setItem('submissionCount', count);
+    console.log("Current submission count: " + count);
+}
+
+document.addEventListener('DOMContentLoaded', function (e) {
+   incrementSubmissionCount();   
+});
